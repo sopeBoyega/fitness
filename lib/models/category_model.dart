1fbb59674a3220c9931b05b98535d6143a705904
s._.ope <sopeadegboyega@gmail.com>
2025-04-16 09:16:01 +0100

    Progress so Far

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -10,4 +10,32 @@ class CategoryModel{
     required this.boxColor
   });
 
+ static List<CategoryModel> getCategories () 
+ {
+  List<CategoryModel> categories  = [];
+
+  categories.add(
+    CategoryModel(name: "Salad",
+    iconPath: "assets/icons/plate.svg",
+    boxColor: Color(0xff92A3FD))
+  );
+  categories.add(
+    CategoryModel(name: "Cake",
+    iconPath: "assets/icons/pancakes.svg",
+    boxColor: Color(0xffC588F2))
+  );
+  categories.add(
+    CategoryModel(name: "Pie",
+    iconPath: "assets/icons/pie.svg",
+    boxColor: Color(0xff92A3FD))
+  );
+  categories.add(
+    CategoryModel(name: "Smoothies",
+    iconPath: "assets/icons/orange-snacks.svg",
+    boxColor: Color(0xffC588F2))
+  );
+
+  return categories;
+ }
+
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,29 @@
+import "package:fitness/models/category_model.dart";
 import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
       appBar: appBar(),
       body: Column(
@@ -28,15 +46,59 @@ class HomePage extends StatelessWidget {
               ),
               SizedBox(height: 15),
               Container(
-                height: 150,
-                color: Colors.green,
+                height: 120,
 
-                child: ListView.builder(itemBuilder: 
-                (context, index) {
-                  return Container();
-                }
+                child: ListView.separated(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  separatorBuilder: (context, index) => SizedBox(width: 25),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: categories[index].boxColor.withValues(
+                          alpha: 0.3,
+                        ),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SvgPicture.asset(
+                                categories[index].iconPath,
+                              ),
+                            ),
+
+                          
+                          ),
+
+                          Text(
+                            categories[index].name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              color:  Colors.black,
+                              fontSize: 14,
+                            ),
+                          )
+
+                         
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              )
+              ),
             ],
           ),
         ],
